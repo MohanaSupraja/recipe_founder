4cdbf0136073919593a4e5852f9ac3b8029aa7be
Toleti Mohana Supraja <toleti.supraja@sifycorp.com>
2025-11-11 15:10:28 +0530

    Initialize git4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Main from './Pages/Main';
-import MealDetails from './Pages/MealDetails';  
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CategorySearch from './Pages/CategorySearch';
 function App() {
-
   return (
-    <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main/>} />
-        <Route path="/category/:category" element={<CategorySearch/>} />
-        <Route path="/meal/:id" element={<MealDetails/>} />
-      </Routes>
-    </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import React from 'react';
+// import Main from './Pages/Main';
+// import MealDetails from './Pages/MealDetails';  
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import CategorySearch from './Pages/CategorySearch';
+// function App() {
+
+//   return (
+//     <>
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Main/>} />
+//         <Route path="/category/:category" element={<CategorySearch/>} />
+//         <Route path="/meal/:id" element={<MealDetails/>} />
+//       </Routes>
+//     </Router>
+//     </>
+//   );
+// }
+
+// export default App;
